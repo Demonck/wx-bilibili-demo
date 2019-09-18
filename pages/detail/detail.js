7244c3dfa83972a18a551b1ce5b232c4eacde5ff
Demonck <953432964@qq.com>
2019-09-18 16:40:49 +0800

    完成视频详情页推荐视频模块

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -13,47 +13,67 @@ Page({
       "author": "牧草君2",
       "playCount": "24.9万",
       "commentCount": 1345,
-      "date": "12-15"},
-      // 推荐视频数据
-    authorRecommendList:[]
+      "date": "12-15"
+    },
+    // 推荐视频数据
+    authorRecommendList: [],
+    //视频评论数据
+    commentList:[]
   },
   /**
    * 根据点击视频的id获取视频详情
    */
   getCurrentVideo(videoId) {
-    let that=this;
+    let that = this;
     wx.request({
-      url: 'https://www.easy-mock.com/mock/5d590978d3185331448bb542/bilibili/videoDetail?id='+videoId,
-      success(res){
-        if(res.statusCode===200){
+      url: 'https://www.easy-mock.com/mock/5d590978d3185331448bb542/bilibili/videoDetail?id=' + videoId,
+      success(res) {
+        if (res.statusCode === 200) {
           that.setData({
-            videoInfo:res.data.data.videoInfo
+            videoInfo: res.data.data.videoInfo
           })
         }
       }
     })
   },
   /**
-   * 使用官方aip，通过视频av号获取作者推荐视频
+   * 使用官方api，通过视频av号获取作者推荐视频
    */
-  getAuthorRecommend(){
-    let that=this;
-    
+  getAuthorRecommend() {
+    let that = this;
+
     wx.request({
       url: 'http://api.bilibili.cn/author_recommend?aid=67971547',
-      success(res){
-        if(res.statusCode===200){
+      success(res) {
+        if (res.statusCode === 200) {
           console.log('获取推荐视频成功！');
+          
           that.setData({
-            authorRecommendList:res.data.list
-            
-          })   
+            authorRecommendList: res.data.list
+          })
         }
       }
     })
-    
-  },
 
+  },
+  /**
+   * 使用官方api，通过视频av号视频评论
+   */
+  getCommentList() {
+    let that = this;
+    wx.request({
+      url: 'http://api.bilibili.com/x/reply?type=1&oid=60839349&pn=0&nohot=1&sort=0',
+      success(res) {
+        if (res.statusCode === 200) {
+          console.log('获取视频评论成功！');
+          console.log(res.data.data.replies[0].replies[0].member);
+          that.setData({
+              commentList:res.data.data
+          })
+        }
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -62,21 +82,23 @@ Page({
     let videoId = options.id;
     //this.getCurrentVideo(videoId);
     this.getAuthorRecommend();
+    this.getCommentList();
     
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function() {
-
+   
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+   
   },
 
   /**
